f2bc9fb8625e565217e973e87247921bc61c8cbe
Ibrahim Mohsen <ibrahim.mohsen.zaki@gmail.com>
2019-12-15 21:13:58 +0200

    Adding POJOs.

---
+++ src/main/java/com/springframework/study/sfgpetclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package com.springframework.study.sfgpetclinic.model;
+
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
