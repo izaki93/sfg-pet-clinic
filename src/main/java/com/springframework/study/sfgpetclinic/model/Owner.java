d718cdd97d218f462e089c2be7f0efa6e35977f0
izaki93 <ibrahim.mohsen.zaki@gmail.com>
2019-12-16 08:47:52 +0200

    Adding POJOs

---
+++ src/main/java/com/springframework/study/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springframework.study.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/springframework/study/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.springframework.study.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
