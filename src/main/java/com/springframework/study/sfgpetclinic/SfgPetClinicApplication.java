a553a68bf1657653020c5de5fda13dcf031e488d
izaki93 <ibrahim.mohsen.zaki@gmail.com>
2019-12-16 08:43:34 +0200

    Initial Add

---
+++ src/main/java/com/springframework/study/sfgpetclinic/SfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.study.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springframework/study/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.study.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
