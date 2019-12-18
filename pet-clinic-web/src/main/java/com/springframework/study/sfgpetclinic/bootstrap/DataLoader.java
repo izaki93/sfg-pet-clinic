d0cd5a69fd122f60654b9efb58a891aa5dc89a8c
izaki93 <ibrahim.mohsen.zaki@gmail.com>
2019-12-18 10:58:30 +0200

    Adding Owner List and update the Controller. Closes #6

--- pet-clinic-web/src/main/java/com/springframework/study/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/study/sfgpetclinic/bootstrap/DataLoader.java
@@ -13,7 +13,7 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
-    
+
     public DataLoader(OwnerService ownerService, VetService vetService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
--- pet-clinic-web/src/main/java/com/springframework/study/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/study/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,29 @@
 package com.springframework.study.sfgpetclinic.controllers;
 
+import com.springframework.study.sfgpetclinic.model.Owner;
+import com.springframework.study.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Set;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String ListOwners(){
+    public String ListOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
