2ca902d1ef814427501f6f59c8ee54ebc1c28fd8
Ibrahim Mohsen <ibrahim.mohsen.zaki@gmail.com>
2020-01-08 21:37:58 +0200

    updated owner with JPA Entity Mapping. Closes #26

--- pet-clinic-data/src/main/java/com/springframework/study/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/springframework/study/sfgpetclinic/model/PetType.java
@@ -1,6 +1,14 @@
 package com.springframework.study.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
+
+    @Column(name = "name")
     private String name;
 
     public String getName() {
