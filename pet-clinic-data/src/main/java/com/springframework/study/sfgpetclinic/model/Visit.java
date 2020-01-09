fe9c536c9611e31859404e418842f14aa076d45f
Ibrahim Mohsen <ibrahim.mohsen.zaki@gmail.com>
2020-01-09 07:57:31 +0200

    updated visit to use JPA. Closes #28

--- pet-clinic-data/src/main/java/com/springframework/study/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/springframework/study/sfgpetclinic/model/Visit.java
@@ -1,11 +1,20 @@
 package com.springframework.study.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table("visit")
 public class Visit extends BaseEntity {
 
+    @Column(name = "name")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
